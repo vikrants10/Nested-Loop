4b7c7a8f32b886ca35b0cc2cd6f124e3bb2f3336
vikrant10 <vikrantkumarsingh333@gmail.com>
2022-11-02 09:36:30 +0530

    Done

---
+++ Iw1.js
@@ -0,0 +1,8 @@
+// Print the Calendar date in the below format
+let i = 1;
+let j = 1;
+for (i=1;i<=31;i++) {
+  for(j=1;j<=12;j++) {
+    console.log(i + "-" , j)
+  }
+}
---
+++ Iw3.js
@@ -0,0 +1,14 @@
+// Print a box patterns using *
+for (let i = 0; i <= 10; i++) {
+  let bag = "";
+  for (let j = 1; j <= 10; j++) {
+    if (i == 0 || i == 10) {
+      bag = bag + "*";
+    } else if (j == 1 || j == 10) {
+      bag = bag + "*";
+    } else {
+      bag = bag + " ";
+    }
+  }
+  console.log(bag);
+}
